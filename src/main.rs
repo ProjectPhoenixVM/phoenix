7ad12f740f1f9332db1e2653f16b47fe310eb43c
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2025-11-16 20:42:55 -0500

    Solved?

--- src/main.rs
+++ src/main.rs
@@ -240,7 +240,7 @@ impl<'a> SampledMultiHashMap<'a> {
         }
     }
 
-    pub fn best_match(&self, page: &AlignedPage) -> PageIndex {
+    pub fn best_match(&self, page: &AlignedPage) -> Option<PageIndex> {
         let mut best_diff = PAGE_SIZE;
         let mut best_index = FULL_PAGE_FLAG;
         for (sampler, map) in zip(self.indices, &self.maps) {
@@ -256,8 +256,7 @@ impl<'a> SampledMultiHashMap<'a> {
                 }
             }
         }
-        // dbg!(best_diff);
-        best_index
+        (best_diff != PAGE_SIZE).then_some(best_index)
     }
 }
 
@@ -267,49 +266,54 @@ fn read_const<const N: usize>(mut reader: impl Read) -> io::Result<[u8; N]> {
     Ok(buf)
 }
 
-const HIGH_BITS: u32 = 16;
-const LOW_BITS: u32 = (MAX_PAGE_INDEX + 1).ilog2() + PAGE_SIZE.ilog2() - HIGH_BITS;
+const COMPRESSED_DIFF_HIGH_BITS: u32 = 16;
+const COMPRESSED_DIFF_LOW_BITS: u32 =
+    (MAX_PAGE_INDEX + 1).ilog2() + PAGE_SIZE.ilog2() - COMPRESSED_DIFF_HIGH_BITS;
 
 #[derive(Default)]
-struct DiffMetadata {
+struct CompressedDiffs {
     packed_metadata: Vec<u64>,
     address_high_bits: Vec<u32>,
+    data: Vec<u8>,
 }
 
-impl DiffMetadata {
+impl CompressedDiffs {
     // Key only uses lower 30 bits, ie. key & PAGE_FLAG_MASK == 0
     // Packed is 30 bits base_page, 8 bits method, 26 bits low address
 
-    pub fn get(&self, key: u32) -> DiffMetadataItem {
+    pub fn get(&self, key: u32) -> CompressedDiffItem<'_> {
         let (curr_high, curr_packed) = self.get_raw_item(key).expect("All keys should be valid");
         let addr = Self::addr(curr_high, curr_packed);
-        let len = self.get_raw_item(key + 1).map(|(high, packed)| {
-            let next_addr = Self::addr(high, packed);
-            (next_addr - addr) as u16
-        });
-        let method = (curr_packed >> LOW_BITS & 0xFF) as CompressionMethod;
-        let base_page = (curr_packed >> LOW_BITS >> 8) as PageIndex;
-        DiffMetadataItem {
-            addr,
-            len,
+        let len = match self.get_raw_item(key + 1) {
+            Some((high, packed)) => {
+                let next_addr = Self::addr(high, packed);
+                next_addr - addr
+            }
+            None => self.data.len() - addr,
+        };
+        let method = (curr_packed >> COMPRESSED_DIFF_LOW_BITS & 0xFF) as CompressionMethod;
+        let base_page = (curr_packed >> COMPRESSED_DIFF_LOW_BITS >> 8) as PageIndex;
+        CompressedDiffItem {
+            data: &self.data[addr..][..len],
             method,
             base_page,
         }
     }
 
-    // Data must be pushed in order of address, previous addr + len should equal current addr
-    pub fn push(&mut self, data: DiffMetadataItem) -> u32 {
+    pub fn push(&mut self, data: CompressedDiffItem) -> u32 {
+        let addr = self.data.len();
         assert!(
-            data.addr < 1 << (LOW_BITS + HIGH_BITS),
+            addr < 1 << (COMPRESSED_DIFF_LOW_BITS + COMPRESSED_DIFF_HIGH_BITS),
             "address should not exceed MAX_PAGE_INDEX * PAGE_SIZE"
         );
-        let high_bits = (data.addr >> LOW_BITS) as u16 & (u16::MAX);
-        let low_bits = data.addr & ((1 << LOW_BITS) - 1);
+        let high_bits = (addr >> COMPRESSED_DIFF_LOW_BITS) as u16 & (u16::MAX);
+        let low_bits = addr & ((1 << COMPRESSED_DIFF_LOW_BITS) - 1);
 
         let new_key = self.packed_metadata.len() as u32;
-        let packed = (data.base_page as u64 & !(PAGE_FLAG_MASK as u64)) << 8 << LOW_BITS
-            | (data.method as u64) << LOW_BITS
-            | low_bits as u64;
+        let packed =
+            (data.base_page as u64 & !(PAGE_FLAG_MASK as u64)) << 8 << COMPRESSED_DIFF_LOW_BITS
+                | (data.method as u64) << COMPRESSED_DIFF_LOW_BITS
+                | low_bits as u64;
         self.packed_metadata.push(packed);
 
         if high_bits as usize > self.address_high_bits.len() + 1 {
@@ -319,13 +323,16 @@ impl DiffMetadata {
         } else if (high_bits as usize) < self.address_high_bits.len() {
             panic!("Addresses should be in order");
         }
+
+        self.data.extend(data.data);
+
         new_key
     }
 
     pub fn write(&self, mut writer: impl Write) -> io::Result<()> {
         writer.write_all(&(self.packed_metadata.len() as u32).to_be_bytes())?;
         writer.write_all(&(self.address_high_bits.len() as u16).to_be_bytes())?;
-        dbg!(self.address_high_bits.len());
+        writer.write_all(&(self.data.len() as u64).to_be_bytes())?;
 
         for packed in &self.packed_metadata {
             writer.write_all(&packed.to_be_bytes())?;
@@ -334,6 +341,8 @@ impl DiffMetadata {
         for high_bits in &self.address_high_bits {
             writer.write_all(&high_bits.to_be_bytes())?;
         }
+
+        writer.write_all(self.data.as_slice())?;
         Ok(())
     }
 
@@ -341,6 +350,7 @@ impl DiffMetadata {
     pub fn read(mut reader: impl Read) -> io::Result<Self> {
         let num_metadata = u32::from_be_bytes(read_const(&mut reader)?);
         let num_high_bits = u16::from_be_bytes(read_const(&mut reader)?);
+        let num_data = u64::from_be_bytes(read_const(&mut reader)?);
         let mut packed_metadata = Vec::with_capacity(num_metadata as usize);
         for _ in 0..num_metadata {
             packed_metadata.push(u64::from_be_bytes(read_const(&mut reader)?));
@@ -349,10 +359,13 @@ impl DiffMetadata {
         for _ in 0..num_high_bits {
             address_high_bits.push(u32::from_be_bytes(read_const(&mut reader)?));
         }
+        let mut data = vec![0; num_data as usize];
+        reader.read_exact(&mut data)?;
 
         Ok(Self {
             packed_metadata,
             address_high_bits,
+            data,
         })
     }
 
@@ -366,53 +379,142 @@ impl DiffMetadata {
     }
 
     fn addr(high_bits: u16, packed: u64) -> usize {
-        let low_bits = packed & ((1 << LOW_BITS) - 1);
-        (high_bits as usize) << LOW_BITS | low_bits as usize
+        let low_bits = packed & ((1 << COMPRESSED_DIFF_LOW_BITS) - 1);
+        (high_bits as usize) << COMPRESSED_DIFF_LOW_BITS | low_bits as usize
     }
 }
 
 #[derive(Debug, Clone, Copy)]
-struct DiffMetadataItem {
-    addr: usize,
-    len: Option<u16>,
+struct CompressedDiffItem<'a> {
+    data: &'a [u8],
     method: CompressionMethod,
     base_page: PageIndex,
 }
 
-impl DiffMetadataItem {
-    pub fn new(addr: usize, len: usize, method: CompressionMethod, base_page: PageIndex) -> Self {
-        Self {
-            addr,
-            len: Some(len as _),
+const COMPRESSED_PAGE_HIGH_BITS: u32 =
+    (MAX_PAGE_INDEX + 1).ilog2() + PAGE_SIZE.ilog2() - COMPRESSED_PAGE_LOW_BITS;
+const COMPRESSED_PAGE_LOW_BITS: u32 = 32 - 8;
+
+#[derive(Default)]
+struct CompressedPages {
+    packed_metadata: Vec<u32>,
+    address_high_bits: Vec<u32>,
+    data: Vec<u8>,
+}
+
+impl CompressedPages {
+    // Key only uses lower 30 bits, ie. key & PAGE_FLAG_MASK == 0
+    // Packed is 8 bits method, 24 bits low address
+
+    pub fn get(&self, key: u32) -> CompressedPageItem<'_> {
+        let (curr_high, curr_packed) = self.get_raw_item(key).expect("All keys should be valid");
+        let addr = Self::addr(curr_high, curr_packed);
+        let len = match self.get_raw_item(key + 1) {
+            Some((high, packed)) => {
+                let next_addr = Self::addr(high, packed);
+                next_addr - addr
+            }
+            None => self.data.len() - addr,
+        };
+        let method = (curr_packed >> COMPRESSED_PAGE_LOW_BITS & 0xFF) as CompressionMethod;
+        CompressedPageItem {
+            data: &self.data[addr..][..len],
             method,
-            base_page,
         }
     }
 
-    fn addr(&self) -> usize {
-        self.addr
+    pub fn push(&mut self, data: CompressedPageItem) -> u32 {
+        let addr = self.data.len();
+        assert!(
+            addr < 1 << (COMPRESSED_PAGE_LOW_BITS + COMPRESSED_PAGE_HIGH_BITS),
+            "address should not exceed MAX_PAGE_INDEX * PAGE_SIZE"
+        );
+        let high_bits = (addr >> COMPRESSED_PAGE_LOW_BITS) as u16 & (u16::MAX);
+        let low_bits = addr & ((1 << COMPRESSED_PAGE_LOW_BITS) - 1);
+
+        let new_key = self.packed_metadata.len() as u32;
+        let packed = (data.method as u32) << COMPRESSED_PAGE_LOW_BITS | low_bits as u32;
+        self.packed_metadata.push(packed);
+
+        if high_bits as usize > self.address_high_bits.len() + 1 {
+            panic!("Unexpected jump in address");
+        } else if high_bits as usize == self.address_high_bits.len() + 1 {
+            self.address_high_bits.push(new_key);
+        } else if (high_bits as usize) < self.address_high_bits.len() {
+            panic!("Addresses should be in order");
+        }
+
+        self.data.extend(data.data);
+
+        new_key
     }
 
-    // If len is None, it means this slice extends to the end of the diff array
-    fn len(&self) -> Option<usize> {
-        self.len.map(|v| v as _)
+    pub fn write(&self, mut writer: impl Write) -> io::Result<()> {
+        writer.write_all(&(self.packed_metadata.len() as u32).to_be_bytes())?;
+        writer.write_all(&(self.address_high_bits.len() as u32).to_be_bytes())?;
+        writer.write_all(&(self.data.len() as u64).to_be_bytes())?;
+
+        for packed in &self.packed_metadata {
+            writer.write_all(&packed.to_be_bytes())?;
+        }
+
+        for high_bits in &self.address_high_bits {
+            writer.write_all(&high_bits.to_be_bytes())?;
+        }
+
+        writer.write_all(self.data.as_slice())?;
+        Ok(())
     }
 
-    fn method(&self) -> CompressionMethod {
-        self.method
+    #[must_use]
+    pub fn read(mut reader: impl Read) -> io::Result<Self> {
+        let num_metadata = u32::from_be_bytes(read_const(&mut reader)?);
+        let num_high_bits = u32::from_be_bytes(read_const(&mut reader)?);
+        let num_data = u64::from_be_bytes(read_const(&mut reader)?);
+        let mut packed_metadata = Vec::with_capacity(num_metadata as usize);
+        for _ in 0..num_metadata {
+            packed_metadata.push(u32::from_be_bytes(read_const(&mut reader)?));
+        }
+        let mut address_high_bits = Vec::with_capacity(num_high_bits as usize);
+        for _ in 0..num_high_bits {
+            address_high_bits.push(u32::from_be_bytes(read_const(&mut reader)?));
+        }
+        let mut data = vec![0; num_data as usize];
+        reader.read_exact(&mut data)?;
+
+        Ok(Self {
+            packed_metadata,
+            address_high_bits,
+            data,
+        })
     }
 
-    fn base_page(&self) -> PageIndex {
-        self.base_page
+    fn get_raw_item(&self, key: u32) -> Option<(u16, u32)> {
+        let &packed = self.packed_metadata.get(key as usize)?;
+        let high_bits = match self.address_high_bits.binary_search(&key) {
+            Ok(v) => v as u16 + 1,
+            Err(v) => v as u16,
+        };
+        Some((high_bits, packed))
     }
+
+    fn addr(high_bits: u16, packed: u32) -> usize {
+        let low_bits = packed & ((1 << COMPRESSED_PAGE_LOW_BITS) - 1);
+        (high_bits as usize) << COMPRESSED_PAGE_LOW_BITS | low_bits as usize
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct CompressedPageItem<'a> {
+    data: &'a [u8],
+    method: CompressionMethod,
 }
 
 #[derive(Default)]
 struct MemoryDiff {
     base_pages: Vec<PageIndex>,
-    full_child_pages: Vec<AlignedPage>,
-    diff_metadata: DiffMetadata,
-    diff_data: Vec<u8>,
+    compressed_diffs: CompressedDiffs,
+    compressed_pages: CompressedPages,
 }
 
 impl MemoryDiff {
@@ -424,25 +526,28 @@ impl MemoryDiff {
         self.base_pages.push(ZERO_PAGE_INDEX);
     }
 
-    pub fn push_full_page(&mut self, page_data: &AlignedPage) {
-        let full_page_index = self.full_child_pages.len() as PageIndex;
-        self.base_pages.push(FULL_PAGE_FLAG | full_page_index);
-        self.full_child_pages.push(*page_data);
+    pub fn push_full_page(&mut self, method: CompressionMethod, compressed_page: &[u8]) {
+        let data = CompressedPageItem {
+            data: compressed_page,
+            method,
+        };
+        let key = self.compressed_pages.push(data);
+        self.base_pages.push(FULL_PAGE_FLAG | key);
     }
 
     pub fn push_diff_page(
         &mut self,
         base_page_index: PageIndex,
         method: CompressionMethod,
-        delta: &[u8],
+        compressed_delta: &[u8],
     ) {
-        let addr = self.diff_data.len();
-        let metadata = DiffMetadataItem::new(addr, delta.len(), method, base_page_index);
-        // dbg!(metadata);
-        // eprintln!("{:?}", delta);
-        let key = self.diff_metadata.push(metadata);
+        let data = CompressedDiffItem {
+            data: compressed_delta,
+            method,
+            base_page: base_page_index,
+        };
+        let key = self.compressed_diffs.push(data);
         self.base_pages.push(DIFF_PAGE_FLAG | key);
-        self.diff_data.extend(delta);
     }
 
     pub fn num_pages(&self) -> u32 {
@@ -462,27 +567,25 @@ impl MemoryDiff {
         let flags = base_page & PAGE_FLAG_MASK;
         let data = base_page & !PAGE_FLAG_MASK;
 
-        if flags == FULL_PAGE_FLAG {
-            return self.full_child_pages.get(data as usize);
-        }
-
         if flags == PARENT_PAGE_FLAG {
             return parent.get(data as usize);
         }
 
+        if flags == FULL_PAGE_FLAG {
+            let key = data;
+            let page = self.compressed_pages.get(key);
+            decompress_page(page.method, page.data, tmp_buf)
+                .expect("Decompression should not fail");
+
+            return Some(&*tmp_buf);
+        }
+
         if flags == DIFF_PAGE_FLAG {
             let key = data;
-            dbg!(data);
-            let metadata = self.diff_metadata.get(key);
-            dbg!(&metadata);
-            let mut compressed_diff = &self.diff_data[metadata.addr()..];
-            if let Some(len) = metadata.len() {
-                compressed_diff = &compressed_diff[..len];
-            }
-            eprintln!("{:?}", compressed_diff);
-            decompress_page(metadata.method(), compressed_diff, tmp_buf)
+            let diff = self.compressed_diffs.get(key);
+            decompress_page(diff.method, diff.data, tmp_buf)
                 .expect("Decompression should not fail");
-            let base_page = parent.get(metadata.base_page() as usize)?;
+            let base_page = parent.get(diff.base_page as usize)?;
             *tmp_buf = page_xor(base_page, &tmp_buf);
 
             return Some(&*tmp_buf);
@@ -492,39 +595,29 @@ impl MemoryDiff {
 
     pub fn write(&self, mut writer: impl Write) -> io::Result<()> {
         writer.write_all(&(self.base_pages.len() as PageIndex).to_be_bytes())?;
-        writer.write_all(&(self.full_child_pages.len() as PageIndex).to_be_bytes())?;
-        self.diff_metadata.write(&mut writer)?;
         for base_page in &self.base_pages {
             writer.write_all(&base_page.to_be_bytes())?;
         }
-        dbg!(self.full_child_pages.len());
-        writer.write_all(&bytemuck::cast_slice(self.full_child_pages.as_slice()))?;
-        writer.write_all(&self.diff_data.as_slice())?;
+        self.compressed_diffs.write(&mut writer)?;
+        self.compressed_pages.write(&mut writer)?;
         Ok(())
     }
 
     #[must_use]
     pub fn read(mut reader: impl Read) -> io::Result<Self> {
         let num_pages = u32::from_be_bytes(read_const(&mut reader)?);
-        let num_full_child_pages = u32::from_be_bytes(read_const(&mut reader)?);
-        let diff_metadata = DiffMetadata::read(&mut reader)?;
         let mut base_pages = Vec::with_capacity(num_pages as usize);
         for _ in 0..num_pages {
             base_pages.push(u32::from_be_bytes(read_const(&mut reader)?));
         }
-        let mut full_child_pages = Vec::with_capacity(num_full_child_pages as usize);
-        let mut buf = AlignedPage::default();
-        for _ in 0..num_full_child_pages {
-            reader.read_exact(&mut buf.0)?;
-            full_child_pages.push(buf);
-        }
-        let mut diff_data = Vec::new();
-        reader.read_to_end(&mut diff_data)?;
+
+        let compressed_diffs = CompressedDiffs::read(&mut reader)?;
+        let compressed_pages = CompressedPages::read(&mut reader)?;
+
         Ok(Self {
             base_pages,
-            full_child_pages,
-            diff_metadata,
-            diff_data,
+            compressed_diffs,
+            compressed_pages,
         })
     }
 }
@@ -580,8 +673,6 @@ fn compress(args: PhoenixCompressArgs) -> anyhow::Result<()> {
     let span = tracing::info_span!("child_processing");
     let _guard = span.enter();
 
-    let mut xor_size_sum = 0;
-    let mut full_diff = 0;
     let mut diff = MemoryDiff::default();
     for page in &child {
         if page.is_zero() {
@@ -592,31 +683,32 @@ fn compress(args: PhoenixCompressArgs) -> anyhow::Result<()> {
             diff.push_exact_match(exact_match);
             continue;
         }
-        let best_match = sample_map.best_match(page);
-        if best_match == FULL_PAGE_FLAG {
-            diff.push_full_page(page);
-            continue;
+
+        let mut compressed_buf = [0; PAGE_SIZE];
+        let compressed_page = compress_page(&page, &mut compressed_buf);
+
+        if let Some(best_match) = sample_map.best_match(page) {
+            let xor = page_xor(page, &sample_map.pages[best_match]);
+
+            let mut compressed_delta_buf = [0; PAGE_SIZE];
+            let compressed_diff = compress_page(&xor, &mut compressed_delta_buf);
+            if compressed_diff.len() < compressed_page.len() - 4 {
+                // 4 is the additional overhead
+                // of diffing
+                diff.push_diff_page(
+                    best_match,
+                    compressed_diff.method,
+                    &compressed_delta_buf[..compressed_diff.len() as usize],
+                );
+                continue;
+            }
         }
 
-        let xor = page_xor(page, &sample_map.pages[best_match]);
-        let xor_size = xor.iter().filter(|b| **b != 0).count();
-        xor_size_sum += xor_size;
-        if xor_size == PAGE_SIZE {
-            full_diff += 1;
-        }
-
-        let mut buf = [0; PAGE_SIZE];
-        let compressed = compress_page(&xor, &mut buf);
-        if compressed.len() > PAGE_SIZE - 8 {
-            diff.push_full_page(page);
-        } else {
-            diff.push_diff_page(best_match, compressed.method, &buf[..compressed.len()]);
-        }
+        diff.push_full_page(
+            compressed_page.method,
+            &compressed_buf[..compressed_page.len() as usize],
+        );
     }
-    dbg!(xor_size_sum);
-    dbg!(full_diff);
-
-    dbg!(diff.diff_data.len());
 
     drop(_guard);
     drop(span);
